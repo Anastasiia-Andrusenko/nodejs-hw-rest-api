855e673e35b516c60dcc7e388b22cfc0be304d6c
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2023-04-29 18:33:47 +0300

    Update sendEmail.js

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -15,16 +15,6 @@ const nodemailerConfig = {
  
 const transport = nodemailer.createTransport(nodemailerConfig);
 
-// transport.sendMail(email)
-//   .then(() => console.log('Email send success'))
-//   .catch(error => console.log(error.message));
-
-  // const email = {
-  //   to: 'nastya.andrus@gmail.com',
-  //   from: 'nastya.andrus@meta.ua',
-  //   subject: 'Test email',
-  //   html: '<h2><strong>TEST EMAIL</strong> from localhost:3000</h2>'
-  // };
 
 const sendEmail = async (data) => {
   const email = {...data, from: 'nastya.andrus@meta.ua'};
